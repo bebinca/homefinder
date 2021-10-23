008a88b7c077b340e7481a372ea74f2ab959ec4f
bebinca <bebincaa@163.com>
2021-10-23 19:41:28 +0800

    删除不必要的测试代码

--- src/components/helpers/shuffleArray.ts
+++
@@ -1,7 +0,0 @@
-export default function shuffleArray(array: Array<any>) {
-    console.log(array);
-    return array
-    .map(a => ({ sort: a['prop1'], value: a }))
-    .sort((a, b) => b.sort - a.sort)
-    .map(a => a.value);
-}
